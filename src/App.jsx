1b51e0fe852fe034a1bb2768162206f219ac274c
icedlatter <158771840+icedlatter@users.noreply.github.com>
2025-09-13 01:47:44 -0400

    new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, {useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import {Auth} from "./components/Auth"
+import Cookies from "universal-cookie";
+
+
+
+const cookies = new Cookies();
 
 function App() {
   //const [count, setCount] = useState(0)
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
+  const [room, setRoom] = useState(null)
+  console.log(setIsAuth)
+  console.log(setRoom)
+  
+  if (!isAuth ) {
+    console.log("not authenticated")
 
-  return (
-    <div>
-      Hello word
-      <Auth />
+    return (
+      <div>
+        Hello woklllrd
+        <Auth />
+      </div>
+    );
+  }
+
+    console.log("authenticated");
+    return (
+    
+    //the first segment of dir is the homepage we will use,
+    //the chatroom segment will be for continuing the tutorial
+    <div> 
+      
+      Home page???
+      {room ? ( 
+        <div>Chat</div>
+      ) : (
+        <div className="room"> 
+          <label> Enter Room Name:</label>
+          <input/>
+          <button> Enter Chat</button>
+
+        </div>
+      )
+             }
     </div>
+  )
+
+  
+}
+
+export default App
+
+    // //for reference in case this is needed? this was the sample code
     // <>
     //   <div>
     //     <a href="https://vite.dev" target="_blank">
@@ -34,7 +77,3 @@ function App() {
     //     Click on the Vite and React logos to learn more
     //   </p>
     // </>
-  )
-}
-
-export default App
