956a9681fd4cb1280cefa4c5df197bb77fe314f5
icedlatter <158771840+icedlatter@users.noreply.github.com>
2025-09-13 06:17:06 -0400

    ygugyguyguy

---
+++ src/firebase-apikey.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-UfFoRcm1WsRGBuoRmTq1IxlbtDbg0rk",
+  authDomain: "cmustudies.firebaseapp.com",
+  projectId: "cmustudies",
+  storageBucket: "cmustudies.firebasestorage.app",
+  messagingSenderId: "901398176265",
+  appId: "1:901398176265:web:b5ee3450da2f9e55104ed8",
+  measurementId: "G-BVNXN60L0Z"
+};
